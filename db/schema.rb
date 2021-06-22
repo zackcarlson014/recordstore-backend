6b9657734df77ee27050a0882278d498498b2688
zackcarlson014 <jzcarlson014@gmail.com>
2021-06-21 21:40:02 -0500

    record scaffold created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_023745) do
+ActiveRecord::Schema.define(version: 2021_06_22_023927) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2021_06_22_023745) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.integer "artist_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_records_on_artist_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_06_22_023745) do
   end
 
   add_foreign_key "artists", "users"
+  add_foreign_key "records", "artists"
+  add_foreign_key "records", "users"
 end
